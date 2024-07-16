439c329993ba81394146a74c8b99fee4ffcf2e98
Travis Wiertzema <4044268+twiertzema@users.noreply.github.com>
2024-07-16 16:43:45 +0000

    Correct tile definitions

--- src/constants/tiles.ts
+++ src/constants/tiles.ts
@@ -1,52 +1,68 @@
 import { capitalize } from "lodash";
 
 export enum MahjongSuit {
-  Bamboo = "bamboo",
-  Characters = "characters",
-  Circles = "circles",
-  Dragons = "dragons",
-  Winds = "winds",
+	Bamboo = "bamboo",
+	Characters = "characters",
+	Circles = "circles",
 }
 
-export const MahjongRank: Record<MahjongSuit, number[] | string[]> = {
-  [MahjongSuit.Bamboo]: [1, 2, 3, 4, 5, 6, 7, 8, 9],
-  [MahjongSuit.Characters]: [1, 2, 3, 4, 5, 6, 7, 8, 9],
-  [MahjongSuit.Circles]: [1, 2, 3, 4, 5, 6, 7, 8, 9],
-  [MahjongSuit.Dragons]: ["green", "red", "white"],
-  [MahjongSuit.Winds]: ["east", "north", "south", "west"],
+export enum MahjongHonor {
+	Dragons = "dragons",
+	Winds = "winds",
+}
+export const MahjongHonorType: Record<MahjongHonor, string[]> = {
+	[MahjongHonor.Dragons]: ["green", "red", "white"],
+	[MahjongHonor.Winds]: ["east", "north", "south", "west"],
 };
 
-export class MahjongTile<
-  Suit extends MahjongSuit = MahjongSuit,
-  Rank = (typeof MahjongRank)[Suit][number],
-> {
-  readonly rank: Rank;
-  readonly suit: Suit;
+export class MahjongSuitTile<Suit extends MahjongSuit = MahjongSuit> {
+	constructor(
+		readonly suit: Suit,
+		readonly rank: number,
+	) {}
 
-  constructor(suit: Suit, rank: Rank) {
-    this.rank = rank;
-    this.suit = suit;
-  }
-
-  toString() {
-    return `${typeof this.rank === "string" ? capitalize(this.rank) : this.rank} of ${capitalize(this.suit)}`;
-  }
+	toString() {
+		return `${this.rank} of ${capitalize(this.suit)}`;
+	}
 }
 
-export const MahjongDeck = [
-  ...MahjongRank[MahjongSuit.Bamboo].map(
-    (rank) => new MahjongTile(MahjongSuit.Bamboo, rank),
-  ),
-  ...MahjongRank[MahjongSuit.Characters].map(
-    (rank) => new MahjongTile(MahjongSuit.Characters, rank),
-  ),
-  ...MahjongRank[MahjongSuit.Circles].map(
-    (rank) => new MahjongTile(MahjongSuit.Circles, rank),
-  ),
-  ...MahjongRank[MahjongSuit.Dragons].map(
-    (rank) => new MahjongTile(MahjongSuit.Dragons, rank),
-  ),
-  ...MahjongRank[MahjongSuit.Winds].map(
-    (rank) => new MahjongTile(MahjongSuit.Winds, rank),
-  ),
-];
+export class MahjongHonorTile<Honor extends MahjongHonor = MahjongHonor> {
+	constructor(
+		readonly honor: Honor,
+		readonly type: string,
+	) {}
+
+	toString() {
+		return `${capitalize(this.type)} of ${capitalize(this.honor)}`;
+	}
+}
+
+export type MahjongTile = MahjongSuitTile | MahjongHonorTile;
+
+export const MahjongDeck: MahjongTile[] = [];
+
+// Add suited tiles.
+for (const suit of [
+	MahjongSuit.Bamboo,
+	MahjongSuit.Characters,
+	MahjongSuit.Circles,
+]) {
+	// Iterate over every rank in that suit...
+	for (let rank = 0; rank < 9; rank++) {
+		// And add 4 tiles for that rank in that suit.
+		for (let i = 0; i < 4; i++) {
+			MahjongDeck.push(new MahjongSuitTile(suit, rank + 1));
+		}
+	}
+}
+
+// Add honor tiles.
+for (const honor of [MahjongHonor.Dragons, MahjongHonor.Winds]) {
+	// Iterate over the types in the honor...
+	for (const honorType of MahjongHonorType[honor]) {
+		// And add 4 tiles for that type of that honor.
+		for (let i = 0; i < 4; i++) {
+			MahjongDeck.push(new MahjongHonorTile(honor, honorType));
+		}
+	}
+}
