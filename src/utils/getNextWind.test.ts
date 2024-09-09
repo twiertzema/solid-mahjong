7a7ecde22d40f3991734f9f51010369cf7785584
Travis Wiertzema <4044268+twiertzema@users.noreply.github.com>
2024-09-09 15:25:09 +0000

    Fixed getNextWind tests

--- src/utils/getNextWind.test.ts
+++ src/utils/getNextWind.test.ts
@@ -2,17 +2,17 @@ import { expect, test } from "vitest";
 import getNextWind from "./getNextWind";
 
 test("east", () => {
-  expect(getNextWind("east")).toBe("north");
+  expect(getNextWind("east")).toBe("south");
 });
 
 test("north", () => {
-  expect(getNextWind("north")).toBe("west");
+  expect(getNextWind("south")).toBe("west");
 });
 
 test("west", () => {
-  expect(getNextWind("west")).toBe("south");
+  expect(getNextWind("west")).toBe("north");
 });
 
 test("south", () => {
-  expect(getNextWind("south")).toBe("east");
+  expect(getNextWind("north")).toBe("east");
 });
