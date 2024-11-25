2ab552ef332896e3a2f72bebb8c8ec6fde1a6bee
Travis Wiertzema <4044268+twiertzema@users.noreply.github.com>
2024-11-25 17:18:46 +0000

    Improved return type of defaultProps util

--- src/utils/solid.ts
+++ src/utils/solid.ts
@@ -4,9 +4,9 @@ import { mergeProps } from "solid-js";
  * A wrapper around {@link mergeProps} that enforces that the default props be
  *   a subset of the props' type.
  */
-export function defaultProps<Props extends object>(
-  defaults: Partial<Props>,
-  props: Props,
-) {
+export function defaultProps<
+  Props extends object,
+  const Defaults extends Partial<Props>,
+>(defaults: Defaults, props: Props) {
   return mergeProps(defaults, props);
 }
